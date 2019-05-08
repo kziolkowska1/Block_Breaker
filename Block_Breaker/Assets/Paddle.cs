46229956622e46694852044a3b5e9efa24f998c2
Karolina Ziółkowska <kar.ziolkowska@gmail.com>
2019-05-08 22:49:14 +0200

    instrukcja 2 i 3 / nie dziala lose collider; ruszanie platformą w połowie

---
+++ Block_Breaker/Assets/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 64f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 63f;
+
+
+
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
