c173d8a3086eb8e1bd13886b1d2888401ac6adda
Karolina Ziółkowska <kar.ziolkowska@gmail.com>
2019-05-30 16:04:20 +0200

    Ball script

---
+++ Block_Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    Vector2 paddleToBallVector;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] bool hasStarted = false;
+
+    private void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+    }
+
+
+    private void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+            rb.simulated = true;
+        }
+
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
