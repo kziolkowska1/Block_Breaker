ef3905aba196f26d5763290e7684b4b7b6702b86
Karolina Ziółkowska <kar.ziolkowska@gmail.com>
2019-05-30 16:45:13 +0200

    Stworzenie ścian oraz edycja dzialania pilki

---
+++ Block_Breaker/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+    public int maxGuessesAllowed = 5;
+    public Text text;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        NextGuess();
+
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (Random.Range(min, max + 1));
+        if (text != null)
+            text.text = guess.ToString();
+        maxGuessesAllowed -= 1;
+        if (maxGuessesAllowed <= 0)
+        {
+            EditorSceneManager.LoadScene("Lose");
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GuessHigher();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GuessLower();
+        }
+    }
+}
