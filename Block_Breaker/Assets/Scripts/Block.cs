e496303bf76d9e27a96a052de67ce9b6a88d0e5c
Karolina Ziółkowska <kar.ziolkowska@gmail.com>
2019-05-30 17:34:52 +0200

    Edycja oraz dodanie skryptów do bloczków

---
+++ Block_Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Rigidbody2D ball;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        ball.velocity = new Vector2(ball.velocity.x, -ball.velocity.y);
+    }
+}
---
+++ Block_Breaker/Assets/Scripts/BlockTwoHits.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTwoHits : MonoBehaviour
+{
+    public Rigidbody2D ball;
+    public Sprite Block;
+    public Sprite BlockBroken1;
+    private SpriteRenderer cc;
+
+    public int hitsNeeded = 2;
+    public int hitsTaken;
+
+
+    void Start()
+    {
+        hitsTaken = 0;
+        cc = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    void Update()
+    {
+        if (hitsTaken == 1)
+        {
+            cc.sprite = BlockBroken1;
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hitsTaken += 1;
+        ball.velocity = new Vector2(ball.velocity.x, -ball.velocity.y);
+
+        if (hitsTaken >= hitsNeeded)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
